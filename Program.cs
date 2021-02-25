4b65932585b9036cfdc9791c1079fa24ce2d8bbc
arnavdots <arnav.jain@dotsquares.com>
2021-02-25 15:18:46 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
